2812cc3c0679da627e7eed6cac8949a2dbbe0269
sobhi el-safady <sobhi.el-safady.1@ens.etsmtl.ca>
2023-06-10 03:48:12 -0400

    Init commit

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
+
+import markdoc from "@astrojs/markdoc";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    customCss: ['/src/assets/custom-styles.css', '@fontsource/roboto'],
+    title: '',
+    logo: {
+      src: '/src/assets/clientflux-logo-white.webp'
+    },
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), sitemap(), markdoc()],
+  output: "server",
+  adapter: cloudflare()
+});
